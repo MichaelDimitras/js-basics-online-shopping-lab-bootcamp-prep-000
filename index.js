1549a3a0e9299bb7d7d1a2225a41b88487559cc6
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-09 01:05:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function viewCart() {
 
     var msg = "In your cart, you have ";
 
-    for(var i = 0; i < cart.legnth; i++){
+    for(var i = 0; i < cart.legnth - 1; i++){
       msg += ` ${cart[i].keys} at $${cart[i]}, `;
     }
 
