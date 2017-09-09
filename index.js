a58eddfd920cf15e6a2c55c2f6bce305bfeb871a
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-09 02:27:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ function viewCart() {
   }
 
 //  console.log(`${msg.slice(0,-2)} and ${getItemName(getCart().length - 1)} at`)
-return `${msg} and ${getItemName(getCart().length - 1)} at $${getItemPrice(getCart().length - 1)}.`;
+return msg;
+//return `${msg} and ${getItemName(getCart().length - 1)} at $${getItemPrice(getCart().length - 1)}.`;
 }
 
 
