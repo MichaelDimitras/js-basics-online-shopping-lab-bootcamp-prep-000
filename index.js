8ab3dcfb8686ff9c4d61a2944455fac75b983e30
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-09 00:36:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ function setCart(c) {
 function addToCart(item) {
  // write your code here
  var price = Math.floor(Math.random() * (100) + 1);
+ 
  var toAdd = {[item]: [price]};
+ 
  setCart([...cart, toAdd]);
+ 
  console.log(`${item} has been added to your cart.`)
  return cart;
 
