49497a4ed00d16db3957878a206b8d4fd39b2e59
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-09 01:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,6 +36,16 @@ function viewCart() {
   else if (getCart().length == 1){
     console.log(`In your cart, you have ${getItemName(0)} at $${getCart()[0][getItemName(0)]}.`)
   }
+
+  else {
+    var msg = "In your cart, you have ";
+
+    for(var i = 0; i < getCart().length - 1; i++){
+      msg += `${getItemName(i)} at $${getCart()[i][getItemName(0)]}`)
+    }
+
+    console.log(`${msg}, and ${getItemName(getCart.length - 1)} at $${getCart()[0][getItemName(getCart.length - 1)]}.`)`)
+  }
 }
 
 function total() {
